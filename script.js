3df1481a7776681b47f0c7ded90e5a851159a56e
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-03-27 23:59:35 -0400

    requisito 9 // 5 continua incompleto

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-
-function pintar (eventoDeOrigem) {
-  let div = eventoDeOrigem.target;
-  let selecionado = document.getElementsByClassName('selected')[0];
-  div.style.backgroundColor = selecionado.classList[1];
+function pintar(eventoDeOrigem) {
+  const div = eventoDeOrigem.target;
+  const selecionado = document.getElementsByClassName('selected')[0];
+  let cor = selecionado.classList[1];
+  div.style.backgroundColor = cor;
 }
 
 for (let x = 0; x < 25; x += 1) {
-let pixel = document.createElement('div');
-pixel.classList = 'pixel';
-pixel.addEventListener('click', pintar)
-let pai = document.getElementById('pixel-board');
-pai.appendChild(pixel);
+  const pixel = document.createElement('div');
+  pixel.classList = 'pixel';
+  pixel.addEventListener('click', pintar)
+  const pai = document.getElementById('pixel-board');
+  pai.appendChild(pixel);
 }
 
-let black = document.getElementsByClassName('color')[0];
-let blue = document.getElementsByClassName('color')[1];
-let red = document.getElementsByClassName('color')[2];
-let green = document.getElementsByClassName('color')[3];
+const black = document.getElementsByClassName('color')[0];
+const blue = document.getElementsByClassName('color')[1];
+const red = document.getElementsByClassName('color')[2];
+const green = document.getElementsByClassName('color')[3];
 
-function addselected (eventoDeOrigem) {
+function addselected(eventoDeOrigem) {
   black.classList.remove('selected');
   blue.classList.remove('selected');
   red.classList.remove('selected');
   green.classList.remove('selected');
   let liClicada = eventoDeOrigem.target;
   liClicada.classList.add('selected');
- }
+}
 
 black.addEventListener('click', addselected);
 blue.addEventListener('click', addselected);
 red.addEventListener('click', addselected);
 green.addEventListener('click', addselected);
+
+function limpar() {
+  for (let x = 0; x < 25; x += 1) {
+    const pixel = document.getElementsByClassName('pixel');
+    pixel[x].style.backgroundColor = 'white';
+  }
+}
+
+const btn = document.getElementById('clear-board');
+btn.addEventListener('click', limpar);
