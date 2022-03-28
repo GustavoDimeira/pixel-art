efc8197ec9671007a5ed00b2d8e78acca4b176a6
Gustavo Dimeira <gustavodimeira1@gmail.com>
2022-03-27 22:48:10 -0400

    requisito 7

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ pixel.classList = 'pixel';
 let pai = document.getElementById('pixel-board');
 pai.appendChild(pixel);
 }
+
+let black = document.getElementsByClassName('color')[0];
+let blue = document.getElementsByClassName('color')[1];
+let red = document.getElementsByClassName('color')[2];
+let green = document.getElementsByClassName('color')[3];
+
+function addselected (eventoDeOrigem) {
+  black.classList.remove('selected');
+  blue.classList.remove('selected');
+  red.classList.remove('selected');
+  green.classList.remove('selected');
+  let liClicada = eventoDeOrigem.target;
+  liClicada.classList.add('selected');
+ }
+black.addEventListener('click',addselected);
+blue.addEventListener('click',addselected);
+red.addEventListener('click',addselected);
+green.addEventListener('click',addselected);
